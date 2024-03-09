d231c3a40dcc35f410b140cabe5ef7f92504dbf8
SoulGiannis <chaudharyrishabh029@gmail.com>
2024-03-10 00:02:45 +0530

    updated index.html

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     return (
         <div className='header'>
             <a href="https://github.com/chaudhary-rishabh"><FontAwesomeIcon className='header-icon' style={{ color: "#fff" }} icon={faGithub} /></a>
-            <a href="https://drive.google.com/file/d/1JI7l2EjiceY4lrUwEU21cCdiGMqCPQpU/view?usp=drive_link"><FontAwesomeIcon className='header-icon' style={{ color: "#375168" }} icon={faFilePdf} /></a>
+            <a href="https://drive.google.com/file/d/1efpjfuL0jzaANv0Lmdk2LEY2t03ibCLV/view?usp=drive_link"><FontAwesomeIcon className='header-icon' style={{ color: "#375168" }} icon={faFilePdf} /></a>
             <a href="mailto:me@chaudharyrishabh.com"><FontAwesomeIcon className='header-icon' style={{ color: "#fff" }} icon={faEnvelope} /></a>
             <a href="https://www.linkedin.com/in/rishabh029/"><FontAwesomeIcon className='header-icon' style={{ color: "#0077B5" }} icon={faLinkedin} /></a>
             <a href="https://linktr.ee/rishabh_chaudhary"><FontAwesomeIcon className='header-icon' style={{ color: "#7289DA" }} icon={faExternalLinkAlt} /></a> {/* Use faExternalLinkAlt */}
