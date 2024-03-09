41c3924b9235ec73d2522ea7fa398d3c26d73b2a
SoulGiannis <chaudharyrishabh029@gmail.com>
2024-03-10 00:34:06 +0530

    updated particles.js onScroll

--- src/Components/Particles.js
+++ src/Components/Particles.js
@@ -1,108 +1,103 @@
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { useEffect, useMemo, useState } from "react";
-// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
-
-
+import { loadSlim } from "@tsparticles/slim";
 
 const ParticlesComponent = (props) => {
+    const [particleColor, setParticleColor] = useState("#fff");
 
-    const [init, setInit] = useState(false);
-    // this should be run only once per application lifetime
     useEffect(() => {
         initParticlesEngine(async (engine) => {
-        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        //await loadAll(engine);
-        //await loadFull(engine);
-        await loadSlim(engine);
-        //await loadBasic(engine);
-        }).then(() => {
-        setInit(true);
+            await loadSlim(engine);
         });
+
+        const handleScroll = () => {
+            const shouldBeBlack = window.scrollY > 0;
+            setParticleColor(shouldBeBlack ? "#000" : "#fff");
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
     }, []);
 
     const particlesLoaded = (container) => {
         console.log(container);
     };
 
-
     const options = useMemo(
         () => ({
-        background: {
-            color: {
-                value: "#000",
+            background: {
+                color: {
+                    value: "#000",
+                },
             },
-        },
-        fpsLimit: 120,
-        interactivity: {
-            events: {
-            onClick: {
-                enable: true,
-                mode: "repulse",
+            fpsLimit: 120,
+            interactivity: {
+                events: {
+                    onClick: {
+                        enable: true,
+                        mode: "repulse",
+                    },
+                    onHover: {
+                        enable: true,
+                        mode: 'grab',
+                    },
+                },
+                modes: {
+                    push: {
+                        distance: 30,
+                        duration: 15,
+                    },
+                    grab: {
+                        distance: 200,
+                    },
+                },
             },
-            onHover: {
-                enable: true,
-                mode: 'grab',
+            particles: {
+                color: {
+                    value: particleColor,
+                },
+                links: {
+                    color: particleColor,
+                    distance: 150,
+                    enable: true,
+                    opacity: 0.3,
+                    width: 1,
+                },
+                move: {
+                    direction: "none",
+                    enable: true,
+                    outModes: {
+                        default: "bounce",
+                    },
+                    random: true,
+                    speed: 1,
+                    straight: false,
+                },
+                number: {
+                    density: {
+                        enable: false,
+                    },
+                    value: 90,
+                },
+                opacity: {
+                    value: 1.0,
+                },
+                shape: {
+                    type: "circle",
+                },
+                size: {
+                    value: { min: 1, max: 2 },
+                },
             },
-            },
-            modes: {
-            push: {
-                distance: 30,
-                duration: 15,
-            },
-            grab: {
-                distance: 300,
-            },
-            },
-        },
-        particles: {
-            color: {
-            value: "#FFFFFF",
-            },
-            links: {
-            color: "#FFFFFF",
-            distance: 150,
-            enable: true,
-            opacity: 0.3,
-            width: 1,
-            },
-            move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-                default: "bounce",
-            },
-            random: true,
-            speed: 1,
-            straight: false,
-            },
-            number: {
-            density: {
-                enable: false,
-            },
-            value: 50,
-            },
-            opacity: {
-            value: 1.0,
-            },
-            shape: {
-            type: "circle",
-            },
-            size: {
-            value: { min: 1, max: 2 },
-            },
-        },
-        detectRetina: true,
+            detectRetina: true,
         }),
-        [],
+        [particleColor]
     );
 
-
-    return <Particles id={props.id} init={particlesLoaded} options={options} />; 
+    return <Particles id={props.id} init={particlesLoaded} options={options} />;
 };
 
 export default ParticlesComponent;
